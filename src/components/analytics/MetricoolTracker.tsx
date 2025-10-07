ee9c37b3cda263b5d7fe5a1c59a180143fa53630
medu-coder <edelarosaortiz@gmail.com>
2025-10-07 18:04:44 +0200

    Metricool tracker v2

--- src/components/analytics/MetricoolTracker.tsx
+++ src/components/analytics/MetricoolTracker.tsx
@@ -2,25 +2,20 @@ import { useEffect } from 'react'
 
 export default function MetricoolTracker() {
   useEffect(() => {
-    // Función para cargar el script de Metricool
-    const loadScript = (callback: () => void) => {
-      const head = document.getElementsByTagName("head")[0]
-      const script = document.createElement("script")
-      script.type = "text/javascript"
-      script.src = "https://tracker.metricool.com/resources/be.js"
-      script.onload = callback
-      // Para compatibilidad con IE (aunque ya no es necesario)
-      if ('onreadystatechange' in script) {
-        (script as any).onreadystatechange = callback
-      }
-      head.appendChild(script)
+    // Script exacto de Metricool
+    function loadScript(a: () => void) {
+      var b = document.getElementsByTagName("head")[0]
+      var c = document.createElement("script")
+      c.type = "text/javascript"
+      c.src = "https://tracker.metricool.com/resources/be.js"
+      // @ts-ignore - onreadystatechange es para compatibilidad con IE
+      c.onreadystatechange = a
+      c.onload = a
+      b.appendChild(c)
     }
-
-    // Cargar el script y inicializar el tracking
-    loadScript(() => {
-      if ((window as any).beTracker) {
-        (window as any).beTracker.t({ hash: "866fa3fecb3089a03788d867302baa03" })
-      }
+    
+    loadScript(function() {
+      (window as any).beTracker.t({ hash: "866fa3fecb3089a03788d867302baa03" })
     })
   }, [])
 
